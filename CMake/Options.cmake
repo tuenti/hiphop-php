99c3c625ace415803bc815d46f2005705de526cf
Daniel Pañeda Rodriguez <dpaneda@tuenti.com>
2012-02-07 13:09:51 +0100

    Changing default cmake options for debug builds

--- CMake/Options.cmake
+++ CMake/Options.cmake
@@ -1,35 +1,29 @@
-#set(CMAKE_BUILD_TYPE Debug)
+set(CMAKE_BUILD_TYPE Debug)
 
 option(ENABLE_LATE_STATIC_BINDING "Enable Late Static Binding" ON)
-
 option(INFINITE_LOOP_DETECTION "Enable Infinite Loop Detection" ON)
 option(INFINITE_RECURSION_DETECTION "Enable Infinite Recursion Detection" ON)
 option(REQUEST_TIMEOUT_DETECTION "Enable Timeout Detection" ON)
-
-option(ALWAYS_ASSERT "Enabled asserts in a release build" OFF)
-option(DEBUG_MEMORY_LEAK "Allow easier debugging of memory leaks" OFF)
-option(DEBUG_APC_LEAK "Allow easier debugging of apc leaks" OFF)
-
-option(HOTPROFILER "Enable support for the hot-profiler" OFF)
 option(HOTPROFILER_NO_BUILTIN "Don't consider builtins for the hot-profiler" OFF)
-option(EXECUTION_PROFILER "Enable the execution profiler" OFF)
-option(ENABLE_FULL_SETLINE "Enable full setline function for debugger and code-coverage" OFF)
-
 option(WANT_FB_LIBMCC "want FB Memcache" OFF)
-
 option(USE_JEMALLOC "Use jemalloc" ON)
-
 option(USE_TCMALLOC "Use tcmalloc (if jemalloc is not used)" ON)
-option(USE_GOOGLE_HEAP_PROFILER "Use Google heap profiler" OFF)
-option(USE_GOOGLE_CPU_PROFILER "Use Google cpu profiler" OFF)
+option(DEBUG_APC_LEAK "Allow easier debugging of apc leaks" OFF)
 
 if (CMAKE_BUILD_TYPE STREQUAL Debug)
     option(ALWAYS_ASSERT "Enabled asserts in a release build" ON)
-    option(DEBUG_MEMORY_LEAK "Allow easier debugging of memory leaks" ON)
-    option(DEBUG_APC_LEAK "Allow easier debugging of apc leaks" ON)
     option(HOTPROFILER "Enable support for the hot-profiler" ON)
+    option(DEBUG_MEMORY_LEAK "Allow easier debugging of memory leaks" ON)
     option(EXECUTION_PROFILER "Enable the execution profiler" ON)
     option(ENABLE_FULL_SETLINE "Enable full setline function for debugger and code-coverage" ON)
     option(USE_GOOGLE_HEAP_PROFILER "Use Google heap profiler" ON)
     option(USE_GOOGLE_CPU_PROFILER "Use Google cpu profiler" ON)
+else ()
+    option(ALWAYS_ASSERT "Enabled asserts in a release build" OFF)
+    option(HOTPROFILER "Enable support for the hot-profiler" OFF)
+    option(DEBUG_MEMORY_LEAK "Allow easier debugging of memory leaks" OFF)
+    option(EXECUTIOFF_PROFILER "Enable the execution profiler" ON)
+    option(ENABLE_FULL_SETLINE "Enable full setline function for debugger and code-coverage" OFF)
+    option(USE_GOOGLE_HEAP_PROFILER "Use Google heap profiler" OFF)
+    option(USE_GOOGLE_CPU_PROFILER "Use Google cpu profiler" OFF)
 endif ()
