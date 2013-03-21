d631cf9f1a1e703094c35d68c949b3d100f740be
Daniel Pañeda <dpaneda@gmail.com>
2013-03-21 15:40:32 +0100

    Fix some tests

--- src/test/test_ext_datetime.cpp
+++ src/test/test_ext_datetime.cpp
@@ -320,10 +320,10 @@ bool TestExtDatetime::test_gmstrftime() {
   int t = f_mktime(0,0,0, 6, 27, 2006);
   VS(f_strftime("%a %A %b %B %c %C %d %D %e %g %G %h %H %I %j %m %M %n %p "
                 "%r %R %S %t %T %u %U %V %W %w %x %X %y %Y %Z %z %%", t),
-                "Tue Tuesday Jun June Tue 27 Jun 2006 12:00:00 AM PDT 20 27 "
+                "Tue Tuesday Jun June Tue 27 Jun 2006 12:00:00 AM CEST 20 27 "
                 "06/27/06 27 06 2006 Jun 00 12 178 06 00 \n AM 12:00:00 AM "
                 "00:00 00 \t 00:00:00 2 26 26 26 2 06/27/2006 12:00:00 AM "
-                "06 2006 PDT -0700 %");
+                "06 2006 CEST +0200 %");
   return Count(true);
 }
 
