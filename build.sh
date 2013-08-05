ec3bf81ad7f1a873e922e96454b655ada52f9994
Daniel Pañeda <dpaneda@gmail.com>
2013-08-05 12:08:22 +0200

    Deactivating creation of debug package

--- build.sh
+++ build.sh
@@ -22,11 +22,11 @@ else
   # installing needed deps
   sudo apt-get update
   sudo apt-get install `cat build_dependencies`
-  rm -rf build && mkdir build && cd build
-  cmake -DCMAKE_BUILD_TYPE=Debug ..
-  make -j 4 package
-  cd ..
-  mv build/*deb .
+  #rm -rf build && mkdir build && cd build
+  #cmake -DCMAKE_BUILD_TYPE=Debug ..
+  #make -j 4 package
+  #cd ..
+  #mv build/*deb .
   rm -rf build && mkdir build && cd build
   cmake ..
   make -j 4 package
