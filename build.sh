8e1906317d4c12394473bc6d6415f9b0821ac0b8
Daniel Pañeda <dpaneda@gmail.com>
2013-11-19 12:28:07 +0100

    Changed build.sh to only make one compilation

--- build.sh
+++ build.sh
@@ -22,12 +22,8 @@ else
   # installing needed deps
   sudo apt-get update
   sudo apt-get install `cat build_dependencies`
-  #rm -rf build && mkdir build && cd build
-  #cmake -DCMAKE_BUILD_TYPE=Debug ..
-  #make -j 4 package
-  #cd ..
-  #mv build/*deb .
   rm -rf build && mkdir build && cd build
+  #cmake -DCMAKE_BUILD_TYPE=Debug ..
   cmake ..
   make -j 4 package
   cd ..
